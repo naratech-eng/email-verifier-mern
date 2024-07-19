5f2c7f23cda9a8269c83aede12ee82040b57f762
snsknarayana@gmail.com <snsknarayana@gmail.com>
2024-07-19 15:42:30 -0400

    modified config files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
